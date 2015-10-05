eea63980fc05db5ef7dc637b3ca10f3443971623
Andre Behrens <andre@rallypoint.com>
2015-10-05 11:04:41 -0400

    Removed this code, as it wasn't really necessary.

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -1,7 +1,4 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-  shouldReloadAll() {
-   return true;
-  }
 });
--- app/guests/new/controller.js
+++ app/guests/new/controller.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  listController: Ember.inject.controller('guests.list'),
   actions: {
     submit() {
       this.get('model').save()
